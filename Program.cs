97a7ffe8a74d8bc6ffef2d255a59b8512213e1ff
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-11 15:02:56 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -1,10 +1,73 @@
-﻿namespace Conditions_AdamIshak
+﻿// Adam Ishak
+using Microsoft.VisualBasic;
+using System.ComponentModel.Design;
+using System.Threading.Tasks.Sources;
+
+namespace Conditions_AdamIshak
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int num1;
+            int num2;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Could you put two numbers in?");
+                    num1 = int.Parse(Console.ReadLine());
+                    num2 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("You did not input a number");
+                }
+            }
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("num1 > num2");
+            }
+            else if (num1 < num2) 
+            {
+                Console.WriteLine("num1 < num2");
+            }
+            else
+            {
+                Console.WriteLine("num1 = num2");
+
+            }
+            if( (num1 > 90) && (num2 < 100 ) )
+            {
+                Console.WriteLine("The letter of score is A");
+            }
+            else if ((num1 > 80) && (num2 < 90) )
+            {
+                Console.WriteLine("The letter of score if B");
+            }
+            else if ((num1 > 70) && (num2 < 80) )
+            {
+                Console.WriteLine("The letter of score is C");
+            }
+            else if ((num1 > 60) && (num2 < 70) )
+            {
+                Console.WriteLine("The letter of this score is D");
+            }
+            else if ((num1 > 0) && (num2 >59) )
+            {
+                Console.WriteLine("The letter of this score is:(F");
+            }
+                // is leap year (year)
+           
+            {
+
+            }
+           
+            
+
         }
     }
 }
